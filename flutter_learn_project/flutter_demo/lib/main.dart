d958592a609269aba4fe2155e25f1955be1fb56b
wt <1546766776@qq.com>
2021-04-30 15:33:20 +0800

    flutter案例提交

--- flutter_learn_project/flutter_demo/lib/main.dart
+++ flutter_learn_project/flutter_demo/lib/main.dart
@@ -3,26 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+/**
+ * Widget:
+ * 有状态的Widget: StatefulWidget 在运行过程中有一些状态(data)需要改变
+ *  无状态的Widget: StatelessWidget 内容是确定没有状态(data)的改变
+ */
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // 此小部件是应用程序的根
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,  //控制右上角debug显隐
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // 这是应用程序的主题
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter 学习项目'),
     );
   }
 }
@@ -30,84 +28,58 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  // 此小部件是应用程序的主页
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding( // 设置padding的块件
+        padding: EdgeInsets.all(10),
+        child: ListView( //列表可下拉
+          children: <Widget>[
+            ProductItem("Apple1","Macbook Product1","https://static-tst.wotao.com/property/label/3308715414e842f8afc69432a912b50e.jpg"),
+            ProductItem("Apple2","Macbook Product2","https://static-tst.wotao.com/property/label/3a16f409792149a4bad2f5d7f5341b1f.jpg"),
+            ProductItem("Apple3","Macbook Product3","https://static-tst.wotao.com/property/label/bb4d03ed67c1447fbc6ae2974fb3d441.jpg"),
+          ],
+        ),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+      ),
+    );
   }
+}
+class ProductItem extends StatelessWidget{
+  final String title;
+  final String desc;
+  final String imgSrc;
+
+  ProductItem(this.title,this.desc,this.imgSrc);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container( //创建一个结合了常见绘制、定位和大小调整小部件的小部件。
+      padding: EdgeInsets.all(2), //padding属性设置
+      margin: EdgeInsets.only(top: 5), //margin属性设置
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 3
+        )
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      child: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text(title,style: TextStyle(fontSize:20),),
+            Text(desc,style: TextStyle(fontSize: 16),),
+            SizedBox(height: 10), //占领一定的间距的小块件
+            Image.network(imgSrc),
           ],
-        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
